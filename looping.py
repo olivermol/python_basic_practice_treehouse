25928cfcc6345565e6c8139c2428db644f844390
olivermol <111631561+olivermol@users.noreply.github.com>
2022-10-02 19:36:40 +0200

    looping.py created

---
+++ looping.py
@@ -0,0 +1,11 @@
+name = input("What's your name? ")
+
+# TODO: Ask the user by name if they understand Python while loops
+
+
+# TODO: Write a while statement that checks if the user doesn't understand while loops
+# TODO: Since the user doesn't understand while loops, let's explain them.
+# TODO: Ask the user again, by name, if they understand while loops.
+
+  
+# TODO: Outside the while loop, congratulate the user for understanding while loops

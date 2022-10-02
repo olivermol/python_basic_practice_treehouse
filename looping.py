1ca0433fc9e1e252e3309dfeec94eae072417097
olivermol <111631561+olivermol@users.noreply.github.com>
2022-10-02 22:11:20 +0200

    started

--- looping.py
+++ looping.py
@@ -1,8 +1,7 @@
 name = input("What's your name? ")
 
 # TODO: Ask the user by name if they understand Python while loops
-
-
+print("Dear {}, You should to know it is a loop" .format(name))
 # TODO: Write a while statement that checks if the user doesn't understand while loops
 # TODO: Since the user doesn't understand while loops, let's explain them.
 # TODO: Ask the user again, by name, if they understand while loops.

66696c16645bc3c3ef5e56cc31eeb4c88347773f
olivermol <111631561+olivermol@users.noreply.github.com>
2022-10-03 21:32:39 +0200

    Cleaned

--- looping.py
+++ looping.py
@@ -2,7 +2,6 @@ name = input("What's your name? ")
 YES_ANSWER = 'yes'
 NO_ANSWER = 'no'
 
-# TODO: Ask the user by name if they understand Python while loops
 print("Dear {}, You should to know it is a loop" .format(name))
 answer = input("{}, now do you understand the looping of Python? \n(Enter yes/no)  " .format(name))
 while answer != YES_ANSWER:
@@ -10,10 +9,3 @@ while answer != YES_ANSWER:
         print("Ok {}, no prob! The looping is a repetaing action" .format(name))
     answer = input("{}, now do you understand the looping of Python? \n(Enter yes/no)  " .format(name))
 print("You are great! It is really simple, right {}?" .format(name))
-
-# TODO: Write a while statement that checks if the user doesn't understand while loops
-# TODO: Since the user doesn't understand while loops, let's explain them.
-# TODO: Ask the user again, by name, if they understand while loops.
-
-  
-# TODO: Outside the while loop, congratulate the user for understanding while loops

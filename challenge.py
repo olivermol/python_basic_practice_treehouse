e1d8072019081c579dc68222566fdde727824d48
olivermol <111631561+olivermol@users.noreply.github.com>
2022-10-03 19:52:58 +0200

    challenge.py added and

--- challenge.py
+++ challenge.py
@@ -16,6 +16,14 @@ print("Hey {}! \nYour number is {}" .format(name, number))
 # If the number is divisible by both 3 and 5, print "is a FizzBuzz number."
 # Otherwise, print "is neither a fizzy or a buzzy number."
 # *********************
+# TODO: Define variables for is_fizz and is_buzz that stores 
+# a Boolean value of the condition. Remember that the modulo operator, %, 
+# can be used to check if there is a remainder.
+
+
+# Using the variables, check the condition of the value, and print the necessary
+# string
+
 fizz_number = number % 3 == 0
 buzz_number = number % 5 == 0
 
@@ -27,12 +35,3 @@ elif buzz_number:
    print("{} is a Buzz Number" .format(number))
 else:
    print("{} is neither a fizzy or a buzzy number")
-
-
-# TODO: Define variables for is_fizz and is_buzz that stores 
-# a Boolean value of the condition. Remember that the modulo operator, %, 
-# can be used to check if there is a remainder.
-
-
-# Using the variables, check the condition of the value, and print the necessary
-# string
